41be5508c5a7ff2758fc844f9f7bfc3e32f3d9e9
theborgh <borghiborghi@gmail.com>
2024-01-11 21:38:30 +0100

    add messages controller

---
+++ src/messages/messages.controller.ts
@@ -0,0 +1,17 @@
+import { Controller, Get, Post } from '@nestjs/common';
+
+@Controller('messages')
+export class MessagesController {
+  @Get()
+  listMessages() {
+  }
+
+  @Post()
+  createMessage() {
+  }
+
+  @Get('/:id') 
+  getMessage() {
+  }
+  
+}
